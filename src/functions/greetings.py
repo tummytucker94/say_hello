a20735c33b4dd6ab6e0e38c91fa632c1aeb78fe5
Jermaine Tucker <jtuck1623@gmail.com>
2025-11-07 05:48:49 -0500

    reformatted code, added logs and time measure

--- src/functions/greetings.py
+++ src/functions/greetings.py
@@ -1,23 +1,5 @@
-import logging
-import time
-
-logging.basicConfig(
-    filename='runtime.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(message)s'
-)
-
-def say_hello(name):
-    if name == " ":
-        name = "there"
-        greeting = "Hello " + name + "!"
-        return greeting
-    elif name == "":
-        name = "there"
-        greeting = "Hello " + name + "!"
-        return greeting
-    else:
-        greeting = "Hello " + name + "!"
-        return greeting
-
-logging.info(f"Greeted user '{name or 'there'}' in {end - start:.4f} seconds")
+def say_hello(user_name):
+    if user_name.strip() == "":
+        user_name = "there"
+    greeting = "Hello: " + user_name + "!"
+    return greeting
--- src/main.py
+++ src/main.py
@@ -1,4 +1,18 @@
 from src.functions.greetings import say_hello
+import logging
+import time
+
+# Configure logging
+logging.basicConfig(
+    filename='runtime.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s'
+)
 
 if __name__ == "__main__":
+    
+    # Measure execution time
+    start = time.time()
     say_hello("Jermaine")
+    end = time.time()
+    
