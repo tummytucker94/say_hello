4d3d89e7b52ff615685d1076e55a385ad94ecc02
Jermaine Tucker <jtuck1623@gmail.com>
2025-11-07 05:55:14 -0500

    corrected code to fit specs

--- src/main.py
+++ src/main.py
@@ -2,17 +2,24 @@ from src.functions.greetings import say_hello
 import logging
 import time
 
-# Configure logging
-logging.basicConfig(
-    filename='runtime.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(message)s'
-)
+
 
 if __name__ == "__main__":
     
+    # Configure logging
+    logging.basicConfig(
+    filename='runtime.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s')
+
+
     # Measure execution time
     start = time.time()
-    say_hello("Jermaine")
+    user_name = input("Enter your name: ")
+    greeting = say_hello(user_name)
     end = time.time()
+
+    # Print and log
+    print(greeting)
+    logging.info(f"Greeted user '{user_name or 'there'}' in {end - start:.4f} seconds")
     
