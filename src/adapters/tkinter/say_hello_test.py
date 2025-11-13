448d7304f56509220f0c280d987dd0944349fa51
Jermaine Tucker <jtuck1623@gmail.com>
2025-11-13 13:42:42 -0500

    organized folders into core and adapters and added the logic for a test tkinter program

---
+++ src/adapters/tkinter/say_hello_test.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+# Create the main window (root)
+root = tk.Tk()
+root.title("Hello Tkinter")
+
+# Add a label
+label = tk.Label(root, text="Hello, Tkinter!")
+label.pack()
+
+# Run the app
+root.mainloop()
